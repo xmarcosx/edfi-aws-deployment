b850f7aa3136c1ef4864b9b3a7dcf3faa3681057
Marcos Alcozer <marcos@alcozer.dev>
2021-05-01 04:00:50 +0000

    Creates Cloud SQL instance

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # terraform-sandbox
+
+This repository uses Terraform to deploy various Google Cloud resources.
+
+## Create Service Account JSON
+* Under "Service account", select "New service account".
+* Give it any name you like.
+* For the Role, choose "Project -> Editor".
+* Leave the "Key Type" as JSON.
+* Click "Create" to create the key to the base of this repo as `service.json`
+
+## Deployment
+
+```bash
+
+terraform init;
+terraform plan \
+    -var="GOOGLE_CLOUD_PROJECT=$GOOGLE_CLOUD_PROJECT" \
+    -var="PG_PASSWORD=$PG_PASSWORD";
+terraform apply \
+    -var="GOOGLE_CLOUD_PROJECT=$GOOGLE_CLOUD_PROJECT" \
+    -var="PG_PASSWORD=$PG_PASSWORD";
+
+```
---
+++ gcp.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.66.1"
+    }
+  }
+}
+
+variable "GOOGLE_CLOUD_PROJECT" {
+  type = string
+}
+
+variable "PG_PASSWORD" {
+  type = string
+}
+
+provider "google" {
+
+  credentials = file("service.json")
+
+  project = var.GOOGLE_CLOUD_PROJECT
+  region  = "us-central1"
+  zone    = "us-central1-c"
+
+}
+
+resource "google_project_service" "sqladmin" {
+  project = var.GOOGLE_CLOUD_PROJECT
+  service = "sqladmin.googleapis.com"
+}
+
+resource "google_project_service" "cloudresourcemanager" {
+  project = var.GOOGLE_CLOUD_PROJECT
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+resource "google_sql_database_instance" "master" {
+  name             = "edfi-ods"
+  database_version = "POSTGRES_11"
+  region           = "us-central1"
+
+  settings {
+    tier = "db-f1-micro"
+    disk_autoresize = true
+    backup_configuration {
+      enabled = true
+      start_time = "08:00"
+    }
+  }
+}
+
+resource "google_sql_database" "EdFi_Admin" {
+  name     = "EdFi_Admin"
+  instance = google_sql_database_instance.master.name
+}
+
+resource "google_sql_database" "EdFi_Security" {
+  name     = "EdFi_Security"
+  instance = google_sql_database_instance.master.name
+}
+
+resource "google_sql_database" "EdFi_Ods" {
+  name     = "EdFi_Ods"
+  instance = google_sql_database_instance.master.name
+}
+
+resource "google_sql_user" "users" {
+  name     = "postgres"
+  instance = google_sql_database_instance.master.name
+  host     = "localhost"
+  password = var.PG_PASSWORD
+}
