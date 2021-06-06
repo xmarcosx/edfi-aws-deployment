589ccecc12f6bae3481b49be95f69f16e6831e99
Marcos Alcozer <marcos@alcozer.dev>
2021-06-05 19:32:09 -0500

    Removes Google Cloud and Azure code

--- azure/import_ods_data.sh
+++
@@ -1,8 +0,0 @@
-psql -h $1 -U postgres@edfi-ods  'EdFi_Admin' < /home/vscode/edfi-ods-admin/EdFi_Admin.sql;
-psql -h $1 -U postgres@edfi-ods  'EdFi_Security' < /home/vscode/edfi-ods-security/EdFi_Security.sql;
-psql -h $1 -U postgres@edfi-ods  'EdFi_Ods' < /home/vscode/edfi-ods-minimal/EdFi.Ods.Minimal.Template.sql;
-
-for FILE in `LANG=C ls /home/vscode/edfi-ods-admin-scripts/PgSql/* | sort -V`
-    do
-        psql -h $1 -U postgres@edfi-ods 'EdFi_Admin' < $FILE 
-    done
